08d50736d6edb1b1f069a981297c8e24fd6f7e52
mxx307 <mxx307slq@gmail.com>
2023-04-11 13:17:49 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use learn::run;
-
+use dotenvy::dotenv;
+use dotenvy_macro::dotenv;
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let flag = dotenv!("FLAG");
+    println!("{:?}", flag);
     run().await
 
 }
